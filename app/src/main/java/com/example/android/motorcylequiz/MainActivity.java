03e637f46bb8137062103505acde0f37a1346a50
Rafał Wawrowicz <rwawrowicz@gmail.com>
2018-02-28 20:52:10 +0100

    RC version

--- app/src/main/java/com/example/android/motorcylequiz/MainActivity.java
+++ app/src/main/java/com/example/android/motorcylequiz/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.motorcylequiz;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -25,16 +26,63 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void question1() {
-        RadioGroup question1RG = findViewById(R.id.radio_group_1);
         RadioButton answer1_3RB = findViewById(R.id.radio_button_1_3);
         if (answer1_3RB.isChecked()){
             points++;
         }
     }
 
+    private void question2() {
+        RadioButton answer2_2RB = findViewById(R.id.radio_button_2_2);
+        if (answer2_2RB.isChecked()){
+            points++;
+        }
+    }
+
+    private void question3() {
+        RadioButton answer3_1RB = findViewById(R.id.radio_button_3_1);
+        if (answer3_1RB.isChecked()){
+            points++;
+        }
+    }
+
+    private void question4() {
+        RadioButton answer4_3RB = findViewById(R.id.radio_button_4_3);
+        if (answer4_3RB.isChecked()){
+            points++;
+        }
+    }
+
+    private void question5() {
+        CheckBox answer5_1CB = findViewById(R.id.checkbox_5_1);
+        CheckBox answer5_2CB = findViewById(R.id.checkbox_5_2);
+        CheckBox answer5_3CB = findViewById(R.id.checkbox_5_3);
+        CheckBox answer5_4CB = findViewById(R.id.checkbox_5_4);
+        CheckBox answer5_5CB = findViewById(R.id.checkbox_5_5);
+        if (answer5_1CB.isChecked()){
+            points--;
+        }
+        if (answer5_2CB.isChecked()){
+            points++;
+        }
+        if (answer5_3CB.isChecked()){
+            points++;
+        }
+        if (answer5_4CB.isChecked()){
+            points--;
+        }
+        if (answer5_5CB.isChecked()){
+            points--;
+        }
+    }
+
     private void countPoints() {
         points = 0;
         question1();
+        question2();
+        question3();
+        question4();
+        question5();
     }
 
     private String prepareMessage(){
@@ -46,6 +94,14 @@ public class MainActivity extends AppCompatActivity {
             message = name + ", you ";
         }
         message += "scored " + points + " points";
+        if (points < 3){
+            message += "\nYour knowledge is... well, let's say that you should still learn someting ;)";
+        }else if ((points >= 3) && (points < 6)) {
+            message += "\nYour knowledge is quite impressive";
+        } else {
+            message += "\nCongratulation! All answers are correct! You are an expert!";
+        }
+
         return message;
     }
 
